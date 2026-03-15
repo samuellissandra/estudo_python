e2e361f9e2d74e1a709518537e2f6785daed52cb
Samuel Lauriano <samuellissandra@gmail.com>
2026-03-15 13:40:56 -0300

    dir: Retorna a lista de nomes (atributos, métodos e variáveis) existentes em um objeto ou no escopo atual.
    help:
    Mostra a documentação de um objeto, função, classe ou módulo.
    Usado para aprender como algo funciona no Python.

---
+++ dir_help/main.py
@@ -0,0 +1,15 @@
+"""
+dir:
+Retorna a lista de nomes (atributos, métodos e variáveis)
+existentes em um objeto ou no escopo atual.
+
+help:
+Mostra a documentação de um objeto, função, classe ou módulo.
+Usado para aprender como algo funciona no Python.
+"""
+
+# exemplo de uso.
+
+# print(dir(str))
+
+# print(help(str))
